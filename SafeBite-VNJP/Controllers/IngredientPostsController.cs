3887b18120958d6e0f0d9a7a881d489ce7d2b628
Ren Shimosato <s-ren-2617@occ.co.jp>
2026-05-22 15:21:55 +0900

    feat: Add login

---
+++ SafeBite-VNJP/Controllers/IngredientPostsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SafeBite_VNJP.Controllers
+{
+    public class IngredientPostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
