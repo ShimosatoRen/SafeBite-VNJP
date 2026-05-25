e7981f71e95c45a0dcaa38262619600811b8b576
Mihiro Kina <k-mihiro-2613@occ.co.jp>
2026-05-25 17:20:30 +0900

    要件定義書

--- SafeBite-VNJP/Controllers/HomeController.cs
+++ SafeBite-VNJP/Controllers/HomeController.cs
@@ -2,24 +2,36 @@ using Microsoft.AspNetCore.Mvc;
 using SafeBite_VNJP.Models;
 using System.Diagnostics;
 
-namespace SafeBite_VNJP.Controllers
+//namespace SafeBite_VNJP.Controllers
+//{
+//    public class HomeController : Controller
+//    {
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        public IActionResult Privacy()
+//        {
+//            return View();
+//        }
+
+//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//        public IActionResult Error()
+//        {
+//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//        }
+//    }
+//}
+using Microsoft.AspNetCore.Mvc;
+
+namespace SafeBiteVNJP.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
