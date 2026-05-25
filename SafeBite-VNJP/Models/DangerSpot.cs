637f3877c6d461794546bda5717904a1b5b76e29
Ren Shimosato <s-ren-2617@occ.co.jp>
2026-05-25 17:35:30 +0900

    Restore base login and DB settings for SafeWalk

---
+++ SafeBite-VNJP/Models/DangerSpot.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SafeBite_VNJP.Models
+{
+    public class DangerSpot
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [Range(1, 5)]
+        public int DangerLevel { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public double? Latitude { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public string? UserId { get; set; }
+    }
+}
